b4b6ae02cda782822e1947691dd9ae5c0656b7d2
Agrim Jain <agrimjain015@gmail.com>
2024-12-26 11:49:45 +0530

    third commit

--- python/pythonProject/my progress/password generator - Day 5.py
+++ python/pythonProject/my progress/password generator - Day 5.py
@@ -8,12 +8,16 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+## easy level ##
 
-for char in range(0, nr_letters):
-   password += random.choice(letters)
-for char in range(0, nr_symbols):
-   password += random.choice(symbols)
-for char in range(0, nr_numbers):
-   password += random.choice(numbers)
-print(password)
+# password = ""
+# for char in range(0, nr_letters):
+#    password += random.choice(letters)
+# for char in range(0, nr_symbols):
+#    password += random.choice(symbols)
+# for char in range(0, nr_numbers):
+#    password += random.choice(numbers)
+# print(password)
+
+## hard level ##
+
