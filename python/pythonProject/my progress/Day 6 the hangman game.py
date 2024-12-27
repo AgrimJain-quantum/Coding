dec2623f74b0be12f863e20f7e8067fb9db635ce
Agrim Jain <agrimjain015@gmail.com>
2024-12-27 10:30:02 +0530

    commit

--- python/pythonProject/my progress/Day 6 the hangman game.py
+++ python/pythonProject/my progress/Day 6 the hangman game.py
@@ -1,28 +1,36 @@
-print("I am thinking of a word")
-print("can you guess it?")
-word = "python"
-guesses = ""
-turns = 10
-while turns > 0:
-    failed = 0
-    for char in word:
-        if char in guesses:
-            print(char)
-        else:
-            print("_")
-            failed += 1
-    if failed == 0:
-        print("you won")
-        break
-    guess = input("guess a character:")
-    guesses += guess
-    if guess not in word:
-        turns -= 1
-        print("wrong")
-        print("you have", + turns, "more guesses")
-        if turns == 0:
-            print("you lose")
-            print("the word was", word)
+# print("I am thinking of a word")
+# print("can you guess it?")
+# word = "python"
+# guesses = ""
+# turns = 10
+# while turns > 0:
+#     failed = 0
+#     for char in word:
+#         if char in guesses:
+#             print(char)
+#         else:
+#             print("_")
+#             failed += 1
+#     if failed == 0:
+#         print("you won")
+#         break
+#     guess = input("guess a character:")
+#     guesses += guess
+#     if guess not in word:
+#         turns -= 1
+#         print("wrong")
+#         print("you have", + turns, "more guesses")
+#         if turns == 0:
+#             print("you lose")
+#             print("the word was", word)
     
 print("hello" + input("what is your name?") + "!") 
+
+what = input("what is your name?")
+how = print("hello" + what + "!") 
+print(len(what)) # length of the name
+
+print(len(input("what is your name?"))) # length of the input statement
+lenght = len(input("what is your name?"))
+print(lenght) # length of the input statement
 
