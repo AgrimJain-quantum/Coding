844ca7ee72e3256a2b0234659bcc187c9c2fb98b
Agrim Jain <agrimjain015@gmail.com>
2024-12-27 22:12:39 +0530

    commit

--- python/pythonProject/my progress/Day 6 the hangman game.py
+++ python/pythonProject/my progress/Day 6 the hangman game.py
@@ -24,35 +24,36 @@
 #             print("you lose")
 #             print("the word was", word)
     
-print("hello" + input("what is your name?") + "!") 
+# print("hello" + input("what is your name?") + "!") 
 
-what = input("what is your name?")
-how = print("hello" + what + "!") 
-print(len(what)) # length of the name
+# what = input("what is your name?")
+# how = print("hello" + what + "!") 
+# print(len(what)) # length of the name
 
-print(len(input("what is your name?"))) # length of the input statement
-lenght = len(input("what is your name?"))
-print(lenght) # length of the input statement
+# print(len(input("what is your name?"))) # length of the input statement
+# lenght = len(input("what is your name?"))
+# print(lenght) # length of the input statement
 
-print("hello"[4])
-print("hello"[0])
-print("hello"[1])
-print("hello"[2])
-print("hello"[3])
-print("hello"[4])
-print("hello"[-1])
-print("23456"[3])
-print(123_456_789) # this is a way to write a number with underscores
-print(123456789)
-print(123456789 + 123456789)
-print(3.14159)
+# print("hello"[4])
+# print("hello"[0])
+# print("hello"[1])
+# print("hello"[2])
+# print("hello"[3])
+# print("hello"[4])
+# print("hello"[-1])
+# print("23456"[3])
+# print(123_456_789) # this is a way to write a number with underscores
+# print(123456789)
+# print(123456789 + 123456789)
+# print(3.14159)
 
-## boolean data type
-print(True)
-print(False)
-print(3 > 5)
-print(3 < 5)
-print(3 == 5)
-print(3 >= 5)
-print(3 <= 5)
-print(123+456 == 123-456)
+# ## boolean data type
+# print(True)
+# print(False)
+# print(3 > 5)
+# print(3 < 5)
+# print(3 == 5)
+# print(3 >= 5)
+# print(3 <= 5)
+# print(123+456 == 123-456)
+print(len("hello"))
