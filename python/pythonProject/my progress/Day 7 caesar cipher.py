6ad3565e5a92cde15cc87fa88d1bb8e268476cf5
Agrim Jain <agrimjain015@gmail.com>
2024-12-28 15:06:33 +0530

    commit

--- python/pythonProject/my progress/Day 7 caesar cipher.py
+++ python/pythonProject/my progress/Day 7 caesar cipher.py
@@ -16,3 +16,27 @@
 # print(int(123.45))
 # print(int("123.45"))
 print("Number of letters in your name: " + str(len(input("What is your name? "))))
+# Integer to float
+# x = 5    # int
+# y = 2.0  # float
+# z = x + y  # Python implicitly converts x to float
+# print(z, type(z))  # Output: 7.0 <class 'float'>
+# print("x+y = " + str(z))
+# # Float to integer
+# x = 5.0    # float
+# y = 2.0    # float
+# z = x + y  # Python implicitly converts x and y to float
+# print(z, type(z))  # Output: 7.0 <class 'float'>
+# print("x+y = " + str(z))
+
+num_str = "42"
+num_int = int(num_str)
+print(num_int, type(num_int))  # Output: 42 <class 'int'>
+
+num = 5
+num_float = float(num)
+print(num_float, type(num_float))  # Output: 5.0 <class 'float'>
+
+num = 10
+num_str = str(num)
+print(num_str, type(num_str))  # Output: "10" <class 'str'>
