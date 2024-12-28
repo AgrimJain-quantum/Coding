be418bde3e01d38eb0b0a4007600494dd10f2748
Agrim Jain <agrimjain015@gmail.com>
2024-12-28 16:18:26 +0530

    print

--- python/pythonProject/my progress/Day 7 caesar cipher.py
+++ python/pythonProject/my progress/Day 7 caesar cipher.py
@@ -15,7 +15,7 @@
 # print(int("123"))   
 # print(int(123.45))
 # print(int("123.45"))
-print("Number of letters in your name: " + str(len(input("What is your name? "))))
+# print("Number of letters in your name: " + str(len(input("What is your name? "))))
 # Integer to float
 # x = 5    # int
 # y = 2.0  # float
@@ -29,14 +29,44 @@ print("Number of letters in your name: " + str(len(input("What is your name? "))
 # print(z, type(z))  # Output: 7.0 <class 'float'>
 # print("x+y = " + str(z))
 
-num_str = "42"
-num_int = int(num_str)
-print(num_int, type(num_int))  # Output: 42 <class 'int'>
+# num_str = "42"
+# num_int = int(num_str)
+# print(num_int, type(num_int))  # Output: 42 <class 'int'>
 
-num = 5
-num_float = float(num)
-print(num_float, type(num_float))  # Output: 5.0 <class 'float'>
+# num = 5
+# num_float = float(num)
+# print(num_float, type(num_float))  # Output: 5.0 <class 'float'>
 
-num = 10
-num_str = str(num)
-print(num_str, type(num_str))  # Output: "10" <class 'str'>
+# num = 10
+# num_str = str(num)
+# print(num_str, type(num_str))  # Output: "10" <class 'str'>
+
+# my_list = [1, 2, 3]
+# my_tuple = tuple(my_list)
+# print(my_tuple, type(my_tuple))  # Output: (1, 2, 3) <class 'tuple'>
+
+# my_tuple = (4, 5, 6)
+# my_list = list(my_tuple)
+# print(my_list, type(my_list))  # Output: [4, 5, 6] <class 'list'>
+
+# pairs = [("a", 1), ("b", 2)]
+# my_dict = dict(pairs)
+# print(my_dict, type(my_dict))  # Output: {'a': 1, 'b': 2} <class 'dict'>
+
+# text = "hello"
+# char_list = list(text)
+# print(char_list)  # Output: ['h', 'e', 'l', 'l', 'o']
+
+# print(6/2)
+# print(6//2)
+# print(6%2)
+# print(2*3)
+
+# text = "hello"
+# char_list = list(text)
+# print(char_list)  # Output: ['h', 'e', 'l', 'l', 'o']
+
+# print(6 // 2)
+# print(6 ** 2)
+w = print(3 * 3 + 3 / 3 - 3)
+print(str(w))
