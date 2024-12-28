235f3d597c9489cfa2b24f4deb2665aadf011e1f
Agrim Jain <agrimjain015@gmail.com>
2024-12-28 21:03:25 +0530

    code change

--- python/pythonProject/my progress/Day 7 caesar cipher.py
+++ python/pythonProject/my progress/Day 7 caesar cipher.py
@@ -1,73 +1,73 @@
-# print(type("Hello"))
-# print(type(1))
-# print(type(1.0))
-# print(type(True))
-# print(type([1, 2, 3]))
-# print(type((1, 2, 3)))
-# print(type({"name": "John", "age": 23}))
-# print(type({1, 2, 3}))
-# print(type(None))
-# print(type(type))
-# print(type(print))
-# print(type(len))
-# print(type(type(1)))
-# int("123")
-# print(int("123"))   
-# print(int(123.45))
-# print(int("123.45"))
-# print("Number of letters in your name: " + str(len(input("What is your name? "))))
-# Integer to float
-# x = 5    # int
-# y = 2.0  # float
-# z = x + y  # Python implicitly converts x to float
-# print(z, type(z))  # Output: 7.0 <class 'float'>
-# print("x+y = " + str(z))
-# # Float to integer
-# x = 5.0    # float
-# y = 2.0    # float
-# z = x + y  # Python implicitly converts x and y to float
-# print(z, type(z))  # Output: 7.0 <class 'float'>
-# print("x+y = " + str(z))
+print(type("Hello"))
+print(type(1))
+print(type(1.0))
+print(type(True))
+print(type([1, 2, 3]))
+print(type((1, 2, 3)))
+print(type({"name": "John", "age": 23}))
+print(type({1, 2, 3}))
+print(type(None))
+print(type(type))
+print(type(print))
+print(type(len))
+print(type(type(1)))
+int("123")
+print(int("123"))   
+print(int(123.45))
+print(int("123.45"))
+print("Number of letters in your name: " + str(len(input("What is your name? "))))
 
-# num_str = "42"
-# num_int = int(num_str)
-# print(num_int, type(num_int))  # Output: 42 <class 'int'>
+x = 5    # int
+y = 2.0  # float
+z = x + y  # Python implicitly converts x to float
+print(z, type(z))  # Output: 7.0 <class 'float'>
+print("x+y = " + str(z))
+# Float to integer
+x = 5.0    # float
+y = 2.0    # float
+z = x + y  # Python implicitly converts x and y to float
+print(z, type(z))  # Output: 7.0 <class 'float'>
+print("x+y = " + str(z))
 
-# num = 5
-# num_float = float(num)
-# print(num_float, type(num_float))  # Output: 5.0 <class 'float'>
+num_str = "42"
+num_int = int(num_str)
+print(num_int, type(num_int))  # Output: 42 <class 'int'>
 
-# num = 10
-# num_str = str(num)
-# print(num_str, type(num_str))  # Output: "10" <class 'str'>
+num = 5
+num_float = float(num)
+print(num_float, type(num_float))  # Output: 5.0 <class 'float'>
 
-# my_list = [1, 2, 3]
-# my_tuple = tuple(my_list)
-# print(my_tuple, type(my_tuple))  # Output: (1, 2, 3) <class 'tuple'>
+num = 10
+num_str = str(num)
+print(num_str, type(num_str))  # Output: "10" <class 'str'>
 
-# my_tuple = (4, 5, 6)
-# my_list = list(my_tuple)
-# print(my_list, type(my_list))  # Output: [4, 5, 6] <class 'list'>
+my_list = [1, 2, 3]
+my_tuple = tuple(my_list)
+print(my_tuple, type(my_tuple))  # Output: (1, 2, 3) <class 'tuple'>
 
-# pairs = [("a", 1), ("b", 2)]
-# my_dict = dict(pairs)
-# print(my_dict, type(my_dict))  # Output: {'a': 1, 'b': 2} <class 'dict'>
+my_tuple = (4, 5, 6)
+my_list = list(my_tuple)
+print(my_list, type(my_list))  # Output: [4, 5, 6] <class 'list'>
 
-# text = "hello"
-# char_list = list(text)
-# print(char_list)  # Output: ['h', 'e', 'l', 'l', 'o']
+pairs = [("a", 1), ("b", 2)]
+my_dict = dict(pairs)
+print(my_dict, type(my_dict))  # Output: {'a': 1, 'b': 2} <class 'dict'>
 
-# print(6/2)
-# print(6//2)
-# print(6%2)
-# print(2*3)
+text = "hello"
+char_list = list(text)
+print(char_list)  # Output: ['h', 'e', 'l', 'l', 'o']
 
-# text = "hello"
-# char_list = list(text)
-# print(char_list)  # Output: ['h', 'e', 'l', 'l', 'o']
+print(6/2)
+print(6//2)
+print(6%2)
+print(2*3)
 
-# print(6 // 2)
-# print(6 ** 2)
+text = "hello"
+char_list = list(text)
+print(char_list)  # Output: ['h', 'e', 'l', 'l', 'o']
+
+print(6 // 2)
+print(6 ** 2)
 w = 3.14
 q = int(w)
 print(q)
