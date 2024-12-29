b446eaf3f59dc4747d9995b78bc41af00424bb64
Agrim Jain <agrimjain015@gmail.com>
2024-12-29 19:14:33 +0530

    commit

--- python/pythonProject/my progress/practice and code sampling.py
+++ python/pythonProject/my progress/practice and code sampling.py
@@ -142,6 +142,8 @@ if height >= 120:
         print("Please pay $5.")
     elif age <= 18:
         print("Please pay $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
         print("Please pay $12.")
 else:
