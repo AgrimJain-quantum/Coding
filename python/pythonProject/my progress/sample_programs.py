1bdac1854c456de2824c98fba76f7b55ad51de81
Agrim Jain <agrimjain015@gmail.com>
2025-01-01 19:10:36 +0530

    update

--- python/pythonProject/my progress/sample_programs.py
+++ python/pythonProject/my progress/sample_programs.py
@@ -44,6 +44,10 @@
 # print(random.uniform(1,5))  
 
 student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+max_score = 0
 for score in student_scores:
-    print(score)
+    if score > max_score:
+        max_score = score
+print(max_score)
+
     
