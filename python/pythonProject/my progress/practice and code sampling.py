17ae8142efcc6629c05828fcb8da4d5e4c71c1eb
Agrim Jain <agrimjain015@gmail.com>
2025-01-01 23:05:01 +0530

    update

--- python/pythonProject/my progress/practice and code sampling.py
+++ python/pythonProject/my progress/practice and code sampling.py
@@ -312,23 +312,26 @@
 # password = ''.join(random.choices(characters, k=12))
 # print(password)  # Example: 'aZ3!bX9@Lm2#'
 
-fruits = ["apple", "banana", "cherry"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie")
-    print(fruit + " juice")
-    print(fruits)
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+#     print(fruit + " juice")
+#     print(fruits)
 
 
-print(range(1, 100))
-print(list(range(1, 100)))
-no_of_students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
-for student in no_of_students:
-    print(student)
+# print(range(1, 100))
+# no_of_students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+# for student in no_of_students:
+#     print(student)
+
+# # total = 0
+# # for no in range(0, 11 ,2):
+# #     total += no
+# # print(total)
 
 total = 0
-for no in range(0, 11 ,2):
-    total += no
+for number in range(1, 101):
+    total += number
 print(total)
-
 
