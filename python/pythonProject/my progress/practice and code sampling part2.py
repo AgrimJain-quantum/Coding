e27c874abf1af14d22323e5cc1241d7a66f8c96a
Agrim Jain <agrimjain015@gmail.com>
2025-01-02 22:21:36 +0530

    commit

---
+++ python/pythonProject/my progress/practice and code sampling part2.py
@@ -0,0 +1,7 @@
+def my_function():
+    print("Hello from a function")
+my_function()
+
+def my_function_with_args(username, greeting):
+    print(f"Hello, {username}, from a function! I wish you {greeting}")
+my_function_with_args("John Doe", "a great day!")
