9680890c287b64fe49727a9b52382bf29697e19a
Agrim Jain <agrimjain015@gmail.com>
2025-01-02 23:17:31 +0530

    commit

--- python/pythonProject/my progress/practice and code sampling part2.py
+++ python/pythonProject/my progress/practice and code sampling part2.py
@@ -5,3 +5,7 @@ my_function()
 def my_function_with_args(username, greeting):
     print(f"Hello, {username}, from a function! I wish you {greeting}")
 my_function_with_args("John Doe", "a great day!")
+
+def sum_two_numbers(a, b):
+    return a + b
+print(sum_two_numbers(5, 7))
