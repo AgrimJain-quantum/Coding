69612b364e4389698fbef5cc5a51c61508dc4149
Agrim Jain <agrimjain015@gmail.com>
2025-01-04 12:59:51 +0530

    update

--- python/pythonProject/my progress/practice and code sampling part2.py
+++ python/pythonProject/my progress/practice and code sampling part2.py
@@ -1,51 +1,50 @@
-def my_function():
-    print("Hello from a function")
-my_function()
+# def my_function():
+#     print("Hello from a function")
+# my_function()
 
-def my_function_with_args(username, greeting):
-    print(f"Hello, {username}, from a function! I wish you {greeting}")
-my_function_with_args("John Doe", "a great day!")
+# def my_function_with_args(username, greeting):
+#     print(f"Hello, {username}, from a function! I wish you {greeting}")
+# my_function_with_args("John Doe", "a great day!")
 
-def sum_two_numbers(a, b):
-    return a + b
-print(sum_two_numbers(5, 7))
+# def sum_two_numbers(a, b):
+#     return a + b
+# print(sum_two_numbers(5, 7))
 
-def print_sum_two_numbers(a, b):
-    print(a + b)
-print_sum_two_numbers(5, 7)
+# def print_sum_two_numbers(a, b):
+#     print(a + b)
+# print_sum_two_numbers(5, 7)
 
-def print_sum_two_numbers(a, b):
-    print(a + b)
-    return a + b
-print(print_sum_two_numbers(5, 7))
+# def print_sum_two_numbers(a, b):
+#     print(a + b)
+#     return a + b
+# print(print_sum_two_numbers(5, 7))
 
-def print_sum_two_numbers(a, b):
-    return a + b
-    print(a + b)            
-print(print_sum_two_numbers(5, 7))
+# def print_sum_two_numbers(a, b):
+#     return a + b
+#     print(a + b)            
+# print(print_sum_two_numbers(5, 7))
 
-def print_sum_two_numbers(a, b):
-    print(a + b)
-    return a + b
-print(print_sum_two_numbers(5, 7))
+# def print_sum_two_numbers(a, b):
+#     print(a + b)
+#     return a + b
+# print(print_sum_two_numbers(5, 7))
 
 
-for i in range(5):
-    print(i)
-else:
-    print("Loop completed")
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Loop completed")
 
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n * factorial(n - 1)
 
-print(factorial(5))  # Output: 120
+# print(factorial(5))  # Output: 120
 
 
 def greet(name):
     print(f"Hello, {name}!")  # Code block for the function
-
 greet("Agrim")
 
 # Output: 0, 1, 2, 3, 4
