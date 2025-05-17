1387b9cd2cf28b0a501db62701039cda8c5868f3
Agrim Jain <agrimjain015@gmail.com>
2025-05-17 20:54:01 +0530

    main

--- python/pythonProject/my progress/random_words.py
+++ python/pythonProject/my progress/random_words.py
@@ -4,7 +4,7 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 placeholder = ''
 for letter in chosen_word:
-    placeholder.append("_")
+    placeholder += '_'
 print(placeholder)
 guess = input("guess a letter: ").lower()
 print(guess)
@@ -13,3 +13,32 @@ for letter in chosen_word:
         print("right")
     else:
         print("wrong")
+def greet_with(name, location):
+    print(f"hello {name}")
+    print(f"what is like in {location}")
+greet_with("agrim", "delhi")
+
+def add(num1: int, num2: int) -> int:
+    """Add two numbers"""
+    num3 = num1 + num2
+
+    return num3
+# Driver code
+num1, num2 = 5, 15
+ans = add(num1, num2)
+print(f"The addition of {num1} and {num2} results {ans}.")
+
+# some more functions
+def is_prime(n):
+    if n in [2, 3]:
+        return True
+    if (n == 1) or (n % 2 == 0):
+        return False
+    r = 3
+    while r * r <= n:
+        if n % r == 0:
+            return False
+        r += 2
+    return True
+print(is_prime(78), is_prime(79))
+
