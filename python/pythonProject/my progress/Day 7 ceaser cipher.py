602f0435757992cc3af778042edba123544f18cd
Agrim Jain <agrimjain015@gmail.com>
2025-05-18 12:02:09 +0530

    update

--- python/pythonProject/my progress/Day 7 ceaser cipher.py
+++ python/pythonProject/my progress/Day 7 ceaser cipher.py
@@ -29,21 +29,29 @@
 # full_name("john", "doe")
 # full_name("jain" , "agrim")
 
-print("hello" [1])
-print("hello" [0])
-print("hello" [2])  
-print("hello" [3])
+# print("hello" [1])
+# print("hello" [0])
+# print("hello" [2])  
+# print("hello" [3])
 
-interger = 123
-float = 1.23
-new_number = interger + float
-print(new_number)
-print(type(new_number))
+# interger = 123
+# float = 1.23
+# new_number = interger + float
+# print(new_number)
+# print(type(new_number))
 
-def f1():
-    a = 1
-    def f2():
-        print(a)
-    f2()
-f1()
-
+# def f1():
+#     a = 1
+#     def f2():
+#         print(a)
+#     f2()
+# f1()
+# def my_function(*args):
+#     for arg in args:
+#         print(arg)
+# my_function(1, 2, 3, 4, 5)
+def my_function(arg1, *args):
+    print("funtion :" , arg1)
+    for arg in args:
+        print("my_secondarg : ", arg)
+my_function(1, 2, 3, 4, 5)
