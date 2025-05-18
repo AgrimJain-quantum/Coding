d0a936ec6b2f87756cc25c9f1b2ad581af630c5c
Agrim Jain <agrimjain015@gmail.com>
2025-05-18 22:55:04 +0530

    commit

--- python/pythonProject/my progress/Day 8 .py
+++ python/pythonProject/my progress/Day 8 .py
@@ -1,39 +1,45 @@
-dictionay = { 
-    "name": "agrim",
-    "age": 20
-}
-print(dictionay["name"])
-print(dictionay["age"])
-this_list = dict( name = "agrim" , age = 20, country = "india")
-print(this_list)
+# dictionay = { 
+#     "name": "agrim",
+#     "age": 20
+# }
+# print(dictionay["name"])
+# print(dictionay["age"])
+# this_list = dict( name = "agrim" , age = 20, country = "india")
+# print(this_list)
 
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-x = car.keys()
-print(x) #before the change
-car["color"] = "white"
-print(x) #after the change
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+# x = car.keys()
+# print(x) #before the change
+# car["color"] = "white"
+# print(x) #after the change
 
-y = car.keys()
-print(y) #after the change
-z = car.values()
-print(z) #after the change
-s = car.items()
-print(s) #after the change
+# y = car.keys()
+# print(y) #after the change
+# z = car.values()
+# print(z) #after the change
+# s = car.items()
+# print(s) #after the change
 
-travel_log = {
-    "france" : [
-        {
-            "cities_visited" : ["paris", "lille", "lyon"],
-            "total_visits" : 12
-        },
-        {
-            "cities_visited" : ["paris", "lille", "lyon"],
-            "total_visits" : 12
-        }
-    ]
-}
-print(travel_log["france"][0]["cities_visited"])
+# travel_log = {
+#     "france" : [
+#         {
+#             "cities_visited" : ["paris", "lille", "lyon"],
+#             "total_visits" : 12
+#         },
+#         {
+#             "cities_visited" : ["paris", "lille", "lyon"],
+#             "total_visits" : 12
+#         }
+#     ]
+# }
+# print(travel_log["france"][0]["cities_visited"])
+
+nested_list = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
