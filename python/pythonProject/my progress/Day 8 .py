66c9c3cf607730f9a84214696e7362dac2154355
Agrim Jain <agrimjain015@gmail.com>
2025-05-18 21:56:45 +0530

    commit

--- python/pythonProject/my progress/Day 8 .py
+++ python/pythonProject/my progress/Day 8 .py
@@ -3,3 +3,20 @@ dictionay = {
     "age": 20
 }
 print(dictionay["name"])
+print(dictionay["age"])
+this_list = dict( name = "agrim" , age = 20, country = "india")
+print(this_list)
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
