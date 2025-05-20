ab5906f69d6380698fd09dabecb862881b981467
Agrim Jain <agrimjain015@gmail.com>
2025-05-20 23:31:43 +0530

    commit

--- python/pythonProject/my progress/Day 11.py
+++ python/pythonProject/my progress/Day 11.py
@@ -14,11 +14,30 @@
 
 def format_name(f_name, l_name):
 #  if f_name == "" or l_name == "":
-  
  formated_f_name = f_name.title()
  formated_l_name = l_name.title()
  return f"hi! {formated_f_name} {formated_l_name}"
-print(format_name(input("enter your first name ? :  "), input("enter your last name ? :   ")))
+print(format_name(str(input("enter your first name ? :  ")), str(input("enter your last name ? :   "))))
+
+def add(a, b):
+
+    # returning sum of a and b
+    return a + b
+
+def is_true(a):
+
+    # returning boolean of a
+    return bool(a)
+
+# calling function
+res = add(2, 3)
+print(res)
+
+res = is_true(2<5)
+print(res)
+
+
+
 
 
 
