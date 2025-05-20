6e7cd7d5077f7a6c1f870ac3ccd39472527be99a
Agrim Jain <agrimjain015@gmail.com>
2025-05-20 10:31:51 +0530

    update

--- python/pythonProject/my progress/Day 10.py
+++ python/pythonProject/my progress/Day 10.py
@@ -1,71 +1,75 @@
-thisdict = {
-    "brand" : "aston martin",
-    "model" : "db11",
-    "year" : 2020,
-}
-thisdict.pop("model")
-print(thisdict)
-thisdict.popitem()
-print(thisdict)
-del(thisdict["brand"])
-print(thisdict)
-this = {
-    "brand" : "aston martin",
-    "model" : "db11",
-    "year" : 2020,
-    "color" : "red",
-    "engine" : "v8",
-    "horsepower" : 503,
-    "torque" : 498,
-    "weight" : 3855,
-    "top_speed" : 208,
-}
-this.copy()
-thisdict = {
-    "brand" : "aston martin",
-    "model" : "db11",
-    "year" : 2020,
-}
-thisdict.clear()
-print(thisdict)
-this.items()
-print(this)
-this.fromkeys(thisdict)
-print(this)
-car = {
-    "brand" : "aston martin",
-    "model" : "db11",
-    "year" : 2020,
-    "color" : "red",
-}
-mydict = car.copy()
-print(mydict)
-qwerty = {
-    "brand" : "aston martin",
-    "model" : "db11",
-    "year" : 2020,
-    "color" : "red",
-    "engine" : ["v8", "v12"],
-    "horsepower" : 503,
-    "weight" : 3855,
-    "top_speed" : 208,
-    "acceleration" : 0.0,
-    "braking" : 0.0,
-    "handling" : 0.0,
-    "comfort" : 0.0,
-}
-qwerty.fromkeys("engine")
-print(qwerty)
-returnq = {
-    "name" : "agrim ",
-    "second_name" : "jain",
-    "age" : 20,
-}
-returnq.popitem()
-print(returnq)  
-returnq.pop("name")
-print(returnq)
-\
-def my_function():
-    return 3 * 2
-print(my_function())
+# thisdict = {
+#     "brand" : "aston martin",
+#     "model" : "db11",
+#     "year" : 2020,
+# }
+# thisdict.pop("model")
+# print(thisdict)
+# thisdict.popitem()
+# print(thisdict)
+# del(thisdict["brand"])
+# print(thisdict)
+# this = {
+#     "brand" : "aston martin",
+#     "model" : "db11",
+#     "year" : 2020,
+#     "color" : "red",
+#     "engine" : "v8",
+#     "horsepower" : 503,
+#     "torque" : 498,
+#     "weight" : 3855,
+#     "top_speed" : 208,
+# }
+# this.copy()
+# thisdict = {
+#     "brand" : "aston martin",
+#     "model" : "db11",
+#     "year" : 2020,
+# }
+# thisdict.clear()
+# print(thisdict)
+# this.items()
+# print(this)
+# this.fromkeys(thisdict)
+# print(this)
+# car = {
+#     "brand" : "aston martin",
+#     "model" : "db11",
+#     "year" : 2020,
+#     "color" : "red",
+# }
+# mydict = car.copy()
+# print(mydict)
+# qwerty = {
+#     "brand" : "aston martin",
+#     "model" : "db11",
+#     "year" : 2020,
+#     "color" : "red",
+#     "engine" : ["v8", "v12"],
+#     "horsepower" : 503,
+#     "weight" : 3855,
+#     "top_speed" : 208,
+#     "acceleration" : 0.0,
+#     "braking" : 0.0,
+#     "handling" : 0.0,
+#     "comfort" : 0.0,
+# }
+# qwerty.fromkeys("engine")
+# print(qwerty)
+# returnq = {
+#     "name" : "agrim ",
+#     "second_name" : "jain",
+#     "age" : 20,
+# }
+# returnq.popitem()
+# print(returnq)  
+# returnq.pop("name")
+# print(returnq)
+# \
+# def my_function():
+#     return 3 * 2
+# print(my_function())
+
+my_list = [1,2,3,4,5]
+my_list.pop(2)
+print(my_list)
