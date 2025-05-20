a69de77666a23a888025798eac97dba8eb29fd43
Agrim Jain <agrimjain015@gmail.com>
2025-05-20 16:43:53 +0530

    commit

--- python/pythonProject/my progress/Day 11.py
+++ python/pythonProject/my progress/Day 11.py
@@ -1,6 +1,5 @@
 def format(f_name, l_name):
-    first_name = f_name.title()
-    second_name = l_name.title()
-    return f"hello {first_name} {second_name}"
+ f_name.title()
+ l_name.title()
 print(format("agrim", "jain"))
 
