b4680a24a80007f8822a4dff59710a01dc49f7d5
Agrim Jain <agrimjain015@gmail.com>
2025-05-20 18:19:09 +0530

    comiit

--- python/pythonProject/my progress/Day 11.py
+++ python/pythonProject/my progress/Day 11.py
@@ -1,5 +1,7 @@
 def format(f_name, l_name):
  print(f_name.title())
  print(l_name.title())
+ formatted_f_name  = f_name.title()
+ formattted_l_name = l_name.title()
+ print(f" hi! {formatted_f_name} {formattted_l_name}")
 print(format("agrim", "jain"))
-
