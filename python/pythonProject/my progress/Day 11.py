93e05a59e536522cd96e745834e8f135c0546bbb
Agrim Jain <agrimjain015@gmail.com>
2025-05-20 22:26:01 +0530

    commit

--- python/pythonProject/my progress/Day 11.py
+++ python/pythonProject/my progress/Day 11.py
@@ -6,16 +6,33 @@
 #  print(f" hi! {formatted_f_name} {formattted_l_name}")
 # print(format("agrim", "jain"))
 
+
+
+
+
+
+
 def format_name(f_name, l_name):
+#  if f_name == "" or l_name == "":
+  
  formated_f_name = f_name.title()
  formated_l_name = l_name.title()
- return f"hi!{formated_f_name} {formated_l_name}"
-new_name = format_name("agrim", "jain")
-print(new_name)
+ return f"hi! {formated_f_name} {formated_l_name}"
+print(format_name(input("enter your first name ? :  "), input("enter your last name ? :   ")))
 
-def function_1(text):
- return text + text
-def function_2(text):
- return text.title()
-output = function_2(function_1("agrim"))
-print(output)
+
+
+
+
+
+
+
+
+# new_name = format_name("agrim", "jain")
+# print(new_name)
+# def function_1(text):
+#  return text + text
+# def function_2(text):
+#  return text.title()
+# output = function_2(function_1("agrim"))
+# print(output)
