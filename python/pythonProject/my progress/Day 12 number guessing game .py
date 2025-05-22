148cb171df3dea7c530f8eb90e5d033928833f19
Agrim Jain <agrimjain015@gmail.com>
2025-05-22 15:51:12 +0530

    coomit

--- python/pythonProject/my progress/Day 12 number guessing game .py
+++ python/pythonProject/my progress/Day 12 number guessing game .py
@@ -1,24 +1,43 @@
-x = 2
-def value_x():
-    x = 3
-    print(f"x inside function: {x}")
+# x = 2
+# def value_x():
+#     x = 3
+#     print(f"x inside function: {x}")
 
-value_x()
-print(f"x outside function: {x}")
+# value_x()
+# print(f"x outside function: {x}")
 
 
-var1 = 5# var1 is in the global namespace 
-def some_func(): # var2 is in the local namespace 
-    var2 = 6
-    def some_inner_func():  # var3 is in the nested local namespace
-      var3 = 7
+# var1 = 5# var1 is in the global namespace 
+# def some_func(): # var2 is in the local namespace 
+#     var2 = 6
+#     def some_inner_func():  # var3 is in the nested local namespace
+#       var3 = 7
     
-count = 5
+# count = 5
 
-def some_method():
-    global count
-    count = count + 1
-    print(count)
+# def some_method():
+#     global count
+#     count = count + 1
+#     print(count)
 
-some_method()
-# Output: 6
+# some_method()
+# # Output: 6
+
+# def some_func():
+#     def some_inner_func():
+#         var = 10
+#         print("Inside inner function:", var)
+#     some_inner_func()
+# print("Outside inner function:", var)
+
+# some_func()
+
+
+def outer_funtion():
+    def inner_funtion():
+        print("this is inner funtion")
+    inner_funtion()
+outer_funtion()
+
+
+
