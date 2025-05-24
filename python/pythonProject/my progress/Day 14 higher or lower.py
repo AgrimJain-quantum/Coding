f36ff025310cd2fe50fb95f6547ea177bf449899
Agrim Jain <agrimjain015@gmail.com>
2025-05-24 19:04:55 +0530

    commit

--- python/pythonProject/my progress/Day 14 higher or lower.py
+++ python/pythonProject/my progress/Day 14 higher or lower.py
@@ -1,3 +1,13 @@
 # todo
 # to shoow the ascii art in the start of the game
 # generate a random data for the game
+# format the data into a prinatable format
+# ask the user to guess which one has more followers
+# check if the user is correct
+## get follower count of the first and second data
+## use if statement to compare the follower counts
+# give feedback to the user
+#  score keeping
+# make the game repeatable
+# making account at position b become the next account at position a 
+
