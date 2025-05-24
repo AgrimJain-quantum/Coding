541da7e14355ba3cf4b1a6e9edc4417f148e460d
Agrim Jain <agrimjain015@gmail.com>
2025-05-24 22:13:02 +0530

    commit

--- python/pythonProject/my progress/Day 14 higher or lower.py
+++ python/pythonProject/my progress/Day 14 higher or lower.py
@@ -19,3 +19,34 @@ def format_data(account):
     account_desc = account["description"]
     account_country = account["country"]
     return f"{account_name} , a {account_desc} , from {account_country}"
+
+def check_answer(user_guess, a_followers , b_followers):
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
+
+
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+if account_a == account_b:
+    account_b = random.choice(data)
+
+
+
+print(f"Compare A: {format_data(account_a)}.")
+print(vs)
+print(f"Against B: {format_data(account_b)}.")
+
+
+
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+
+
+
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
