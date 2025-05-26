85405bb202bdf32f89d126adba01c50d1968a280
Agrim Jain <agrimjain015@gmail.com>
2025-05-26 11:28:36 +0530

    commit

--- python/pythonProject/my progress/Day 16.py
+++ python/pythonProject/my progress/Day 16.py
@@ -1,1 +1,14 @@
 import turtle
+timmy = turtle.Turtle()
+def draw_square(size):
+    for _ in range(4):
+        timmy.forward(size)
+        timmy.right(90)
+
+def draw_dashed_line(length, dash_length):
+    for _ in range(length // (2 * dash_length)):
+        timmy.forward(dash_length)
+        timmy.penup()
+        timmy.forward(dash_length)
+        timmy.pendown()
+
