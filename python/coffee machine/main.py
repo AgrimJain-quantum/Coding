e261210053562aab2f5cf6afe0b46845f91132b5
Agrim Jain <agrimjain015@gmail.com>
2025-05-28 10:54:23 +0530

    commit

--- python/coffee machine/main.py
+++ python/coffee machine/main.py
@@ -3,5 +3,19 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 money_machine = MoneyMachine()
 money_machine.report()
+menu = Menu()
+is_on = True
 coffee_maker = CoffeeMaker()
 coffee_maker.report()
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
+        
