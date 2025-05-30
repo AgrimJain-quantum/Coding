629b58319bb0d5527a97605d46b75d6597dfc49c
Agrim Jain <agrimjain015@gmail.com>
2025-05-30 21:48:01 +0530

    commit

---
+++ python/quiz project/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+
+        self.text
+        self.amswer
+        self.question
+new_q = Question("Is Python a programming language?", "Yes")
+
