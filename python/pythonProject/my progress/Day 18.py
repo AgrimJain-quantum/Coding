b4c116fbef7b363c206917475128f39d90e037aa
Agrim Jain <agrimjain015@gmail.com>
2025-05-31 21:20:17 +0530

    commit

--- python/pythonProject/my progress/Day 18.py
+++ python/pythonProject/my progress/Day 18.py
@@ -1,7 +1,7 @@
 from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("blue4")
+timmy_the_turtle.color("oliveDrab4")
 timmy_the_turtle.forward(100)   
 timmy_the_turtle.right(90)
 timmy_the_turtle.forward(100)
