30e9c60dfe8be008496e651872665d6a040f1a6e
Agrim Jain <agrimjain015@gmail.com>
2025-05-31 21:45:48 +0530

    commit

--- python/pythonProject/my progress/Day 18.py
+++ python/pythonProject/my progress/Day 18.py
@@ -1,15 +1,32 @@
-from turtle import Turtle, Screen
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("oliveDrab4")
-timmy_the_turtle.forward(100)   
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+# from turtle import Turtle, Screen
+# timmy_the_turtle = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("oliveDrab4")
+# timmy_the_turtle.forward(100)   
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
 
 
+# screen = Screen()
+# screen.exitonclick()
+from turtle import *
+from random import *
+import turtle as t
+tim = Turtle()
+tom = Turtle()
+terry = Turtle()
+tim.shape("turtle")
+tom.shape("turtle")
+terry.shape("turtle")
+tim.color("red")
+tom.color("blue")
+terry.color("green")
+tim.forward(100)
+tom.forward(100)
+terry.forward(100)
 screen = Screen()
 screen.exitonclick()
