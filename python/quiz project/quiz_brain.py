fbb4c65e0b366bb8758fc72cc64592965ece3da8
Agrim Jain <agrimjain015@gmail.com>
2025-05-31 12:17:22 +0530

    commit

--- python/quiz project/quiz_brain.py
+++ python/quiz project/quiz_brain.py
@@ -15,10 +15,16 @@ class Quizbrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lowwer():
+            self.score += 1
+            self.question_number += 1
             print("you got it right ")
         else:
             print("that's wrong")
         print(f"the correct answer was: {correct_answer}")
-        print(f"Your current score is: {self.question_number}/[len(self.q_list)]")
+        print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
-x``
+        if self.still_has_questions():
+            self.next_question()
+        else:
+            print("You've completed the quiz!")
+            print(f"Your final score was: {self.score}/{self.question_number}")
