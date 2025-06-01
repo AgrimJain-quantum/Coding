cd0d565d082ccc5a3590f387a864d49efa17d461
Agrim Jain <agrimjain015@gmail.com>
2025-06-01 12:57:37 +0530

    commit

--- python/hirst painting/main.py
+++ python/hirst painting/main.py
@@ -1,3 +1,9 @@
 import colorgram
+rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
-print(colors)
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb_colors.append((r,g,b))
