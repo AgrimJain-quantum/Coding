8e3119c69d4b1a4e664751e2b69b2e6c1e058218
Agrim Jain <agrimjain015@gmail.com>
2025-06-01 22:57:56 +0530

    commit

--- python/pythonProject/my progress/Day 19.py
+++ python/pythonProject/my progress/Day 19.py
@@ -4,19 +4,17 @@ screen = Screen()
     
 def move_forwards():
     tim.forward(10)
-    
-
-
-
-
-
-
-
-
-
-
+def move_backwards():
+    tim.backward(10)
+def turn_left():
+    tim.left(10)   
+def turn_right():
+    tim.right(10)
 screen.listen()
-screen.onkey(key = "space" , fun = move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
 screen.exitonclick()
 
 
