1210c410dfb1c657140de2f381e9109d7ae84ba3
Agrim Jain <agrimjain015@gmail.com>
2025-06-01 10:29:40 +0530

    commit

--- python/pythonProject/my progress/Day 18.py
+++ python/pythonProject/my progress/Day 18.py
@@ -21,30 +21,36 @@
 
 import turtle as t
 import random
-tim = t.Turtle()
-#Challenge 1 - draw a square
-tim.fd(100)
-tim.right(90)
-tim.fd(100)
-tim.right(90)
-tim.fd(100)
-tim.right(90)
-tim.fd(100)
-#Challenge 2 - draw a dashed line
-for _ in range(15):
-    tim.fd(10)
-    tim.penup()
-    tim.fd(10)
-    tim.pendown()
-#Challenge 3 - draw shapes
+# tim = t.Turtle()
+# #Challenge 1 - draw a square
+# tim.fd(100)
+# tim.right(90)
+# tim.fd(100)
+# tim.right(90)
+# tim.fd(100)
+# tim.right(90)
+# tim.fd(100)
+# #Challenge 2 - draw a dashed line
+# for _ in range(15):
+#     tim.fd(10)
+#     tim.penup()
+#     tim.fd(10)
+#     tim.pendown()
+# #Challenge 3 - draw shapes
+# colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+# for shape_side_n in range(3, 10):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+# screen = t.Screen()
+# screen.exitonclick()
+#Challenge 4 - Random Walk
+timm = t.Turtle()
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-for shape_side_n in range(3, 10):
-    tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
-screen = t.Screen()
-screen.exitonclick()
+directions = [0, 90, 180, 270]
+timm.fd(30)
+timm.setheading(random.choice(directions))
