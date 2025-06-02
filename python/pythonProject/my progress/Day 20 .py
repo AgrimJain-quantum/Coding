c90e67fee3d62427e542e23718d0594d056eae11
Agrim Jain <agrimjain015@gmail.com>
2025-06-02 15:10:26 +0530

    commit

---
+++ python/pythonProject/my progress/Day 20 .py
@@ -0,0 +1,24 @@
+# to do
+# create a snake body
+# how to move the snake
+# detect collision with food
+# create a score board
+# detect the collision with the wall
+# detect the colision with the tail
+from turtle import Turtle, Screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+segment_1 = Turtle("square")
+segment_1.color("white")
+
+
+
+
+
+
+
+screen.exitonclick()
+ 
