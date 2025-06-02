ec0d5449dede386363677e7cc18e066ea867e90c
Agrim Jain <agrimjain015@gmail.com>
2025-06-02 13:03:50 +0530

    commit

--- python/pythonProject/my progress/Day 19 turtle race.py
+++ python/pythonProject/my progress/Day 19 turtle race.py
@@ -5,16 +5,24 @@ screen.setup(width=500, height = 400)
 user_bet = screen.textinput(title = "make your bet" , prompt = "Which turtle will win the race? Enter a color:")
 color = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
+    
+    
+    
 for turtle_index in range(0, 6):
-    tim = Turtle(shape = "turtle")
-    tim.color(color[turtle_index])
-    tim.penup()
-    tim.goto(x = -230, y = y_positions[turtle_index])
+    new_turtle = Turtle(shape = "turtle")
+    new_turtle.color(color[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x = -230, y = y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 if user_bet:
     is_race_on = True
 while is_race_on:
-    random_distance = random.randint(0,10)
-
-
+    for turtle in all_turtles:
+        if turtle.xcor() > 230: 
+            print(f"The {turtle.pencolor()} turtle won!")
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
+    
 screen.exitonclick()
