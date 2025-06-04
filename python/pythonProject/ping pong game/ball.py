1afe0b618061ea79570ffc76dda8d6122f6a4348
Agrim Jain <agrimjain015@gmail.com>
2025-06-04 20:23:54 +0530

    commit

--- python/pythonProject/ping pong game/ball.py
+++ python/pythonProject/ping pong game/ball.py
@@ -16,5 +16,7 @@ class Ball(Turtle):
         self.y_move *= -1
     def bounce_x(self):
         self.x_move *= -1    
-        
-        
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.bounce_y()
--- python/pythonProject/ping pong game/main.py
+++ python/pythonProject/ping pong game/main.py
@@ -39,14 +39,17 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
     # detect collision with r_paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    # detect when paddle misses the ball
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.reset_position()
         
 
 
 
 
-
+ 
 
 
 
