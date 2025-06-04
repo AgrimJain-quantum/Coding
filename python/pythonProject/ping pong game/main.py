2ea58eadc3aeea67d395ffa48a0e150535a6afdd
Agrim Jain <agrimjain015@gmail.com>
2025-06-04 22:06:06 +0530

    commit

--- python/pythonProject/ping pong game/main.py
+++ python/pythonProject/ping pong game/main.py
@@ -12,6 +12,7 @@ keep the score
 from turtle import *
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 screen = Screen()
 screen.bgcolor("black")
@@ -21,7 +22,7 @@ screen.title("Pong Game")
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
     
 screen.listen()
@@ -32,7 +33,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.01)
     screen.update()
     ball.move()
     
@@ -42,9 +43,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
     # detect when paddle misses the ball
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380 :
         ball.reset_position()
+        scoreboard.l_point()
         
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 
