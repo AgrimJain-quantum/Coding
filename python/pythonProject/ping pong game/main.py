863543fe33121b837298e1087f993a98ef848377
Agrim Jain <agrimjain015@gmail.com>
2025-06-04 10:01:37 +0530

    commit

--- python/pythonProject/ping pong game/main.py
+++ python/pythonProject/ping pong game/main.py
@@ -9,39 +9,13 @@ detect collision with the wall and bounce
 detect when paddle misses the ball
 keep the score
 '''
-from turtle import *
-
+from turtle import Screen
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.title("Ping Pong Game")
-screen.tracer(0)  
-
-r_paddle = Paddle((350, 0))  # Right paddle
-l_paddle = Paddle((-350, 0))  # Left paddle
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.setup(width = 800, height= 600)
+screen.title("Ping Pong Game")  
 
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    
 
-screen.exitonclick()  
+
+screen.exitonclick()
