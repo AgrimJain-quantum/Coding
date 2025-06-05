6b3d2727e4aa74d7ab579e47770cb7ecd6ae9cdf
Agrim Jain <agrimjain015@gmail.com>
2025-06-05 14:28:13 +0530

    commit

--- python/turtle crossing/main.py
+++ python/turtle crossing/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard  =  Scoreboard()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
@@ -18,6 +19,7 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
     car_manager.create_car()
     car_manager.move_cars()
     
@@ -25,13 +27,14 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
+            
 # detect the tutrtle crossing the finish line
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
-    
-
-
+        scoreboard.increase_level()
 # creare a scoreboard
+        
 
 screen.exitonclick()
