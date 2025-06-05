0b3b436f8383b32d6428691a72267c123501cc60
Agrim Jain <agrimjain015@gmail.com>
2025-06-05 22:03:20 +0530

    commit

--- python/snake game/main.py
+++ python/snake game/main.py
@@ -33,14 +33,13 @@ while game_is_on:
        
     # Check for collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.goto(0, 0)
-        scoreboard.write("Game Over", align="center", font=("Arial", 24, "normal"))
+        scoreboard.reset()
     # detect collision with the tail
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+        if segment == snake.head:
+            pass
+        elif  snake.head.distance(segment) < 10:
+            scoreboard.reset()
 screen.exitonclick()   
 screen.bye()
 
--- python/snake game/scoreboard.py
+++ python/snake game/scoreboard.py
@@ -23,11 +23,7 @@ class Scoreboard (Turtle):
         self.score = 0
         self.update_scoreboard()
         
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("Game Over", align = ALIGN, font = FONT)
-        
-        
+              
     def increase_score(self):   
         self.score += 1
         self.update_scoreboard()
