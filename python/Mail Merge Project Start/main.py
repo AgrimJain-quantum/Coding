d191ea9b89fabe36869ca361db7d1b6aa4fe7f88
Agrim Jain <agrimjain015@gmail.com>
2025-06-07 12:35:59 +0530

    commit

--- python/Mail Merge Project Start/main.py
+++ python/Mail Merge Project Start/main.py
@@ -4,8 +4,10 @@ with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\Mail Merge Project Start\I
     names = names_file.readlines()
 
 with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\Mail Merge Project Start\Input\Letters\starting_letter.txt") as letter_file:
-    letter_contets = letter_file.read()
+    letter_contents = letter_file.read()
     for name in names:
-        letter.
-    
-    
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        with open(rf"C:\Users\Agrim Jain\Desktop\Coding\python\Mail Merge Project Start\Output\ReadyToSend\letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+        
