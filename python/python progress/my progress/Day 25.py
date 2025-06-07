749e61d6511d85163030e9a9d50cacb3de0cf19e
Agrim Jain <agrimjain015@gmail.com>
2025-06-07 21:37:20 +0530

    commit

--- python/python progress/my progress/Day 25.py
+++ python/python progress/my progress/Day 25.py
@@ -1,3 +1,8 @@
-with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
-    data = file.readlines()
+# with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
+#     data = file.readlines()
+#     print(data)
+
+import csv
+with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
+    data = csv.reader(file)
     print(data)
