98aeff3750771fad893da7b5f154acd873150e64
Agrim Jain <agrimjain015@gmail.com>
2025-06-07 12:02:58 +0530

    commit

---
+++ python/Mail Merge Project Start/main.py
@@ -0,0 +1,11 @@
+PLACEHOLDER = "[name]"
+
+with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\Mail Merge Project Start\Input\Names\invited_names.txt")as names_file:
+    names = names_file.readlines()
+
+with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\Mail Merge Project Start\Input\Letters\starting_letter.txt") as letter_file:
+    letter_contets = letter_file.read()
+    for name in names:
+        letter.
+    
+    
