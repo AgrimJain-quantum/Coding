fa55e7c42668ecada7c2575e42984a8389933805
Agrim Jain <agrimjain015@gmail.com>
2025-06-07 10:43:16 +0530

    commit

--- python/snake game/scoreboard.py
+++ python/snake game/scoreboard.py
@@ -5,8 +5,8 @@ class Scoreboard (Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open ("data.txt", "r") as data:
-            data.read()
+        with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\snake game\data.txt", mode = "r") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270) 
@@ -20,6 +20,8 @@ class Scoreboard (Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\snake game\data.txt", mode = "w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
         
