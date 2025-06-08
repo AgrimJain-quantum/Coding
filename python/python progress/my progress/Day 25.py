8f9c7983ed84f59c3e69b0d8b0b4cc427b2c571b
Agrim Jain <agrimjain015@gmail.com>
2025-06-08 22:37:08 +0530

    commit

--- python/python progress/my progress/Day 25.py
+++ python/python progress/my progress/Day 25.py
@@ -1,40 +1,62 @@
-# # # # with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
-# # # #     data = file.readlines()
-# # # #     print(data)
+# # # # # with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
+# # # # #     data = file.readlines()
+# # # # #     print(data)
 
-# # # import csv
-# # # with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
-# # #     data = csv.reader(file)
-# # #     temperature = []
-# # #     for row in data:
-# # #         if row[1] != "temp":
-# # #             temperature.append(int(row[1]))
-# # #     print(temperature)
+# # # # import csv
+# # # # with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
+# # # #     data = csv.reader(file)
+# # # #     temperature = []
+# # # #     for row in data:
+# # # #         if row[1] != "temp":
+# # # #             temperature.append(int(row[1]))
+# # # #     print(temperature)
+
+# # # # import pandas as pd
+# # # # data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv")
+# # # # print(type(data["temp"]))
+# # # # print(data["temp"].mean())
 
 # # # import pandas as pd
-# # # data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv")
-# # # print(type(data["temp"]))
-# # # print(data["temp"].mean())
+# # # a = [1, 7, 2]
+# # # myvar = pd.Series(a)
+# # # print(myvar)
+# # # print(myvar[0])
+# # # print(myvar[1])
+# # # print(myvar[2])
+# # # print(myvar[1:3])
+# # # # print(myvar[1:])  # Slicing from index 1 to the end
 
 # # import pandas as pd
 # # a = [1, 7, 2]
-# # myvar = pd.Series(a)
-# # print(myvar)
-# # print(myvar[0])
-# # print(myvar[1])
-# # print(myvar[2])
-# # print(myvar[1:3])
-# # # print(myvar[1:])  # Slicing from index 1 to the end
+# # myvar = pd.Series(a, index = ["x", "y", "z"])
+# # # print(myvar)
 
 # import pandas as pd
-# a = [1, 7, 2]
-# myvar = pd.Series(a, index = ["x", "y", "z"])
-# # print(myvar)
+# data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv")
+# print(type(data["temp"]))
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# x = data["day"]
+# print(x)
+
 
 import pandas as pd
-data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv")
-print(type(data["temp"]))
-print(data["temp"].mean())
-print(data["temp"].max())
-x = data["day"]
-print(x)
+months_days = {    "January": 31,
+    "February": 28,
+    "March": 31,
+    "April": 30,
+    "May": 31,
+    "June": 30,
+    "July": 31,
+    "August": 31,
+    "September": 30,
+    "October": 31,
+    "November": 30,
+    "December": 31
+    }
+
+var = pd.Series(months_days)
+print(var)
+var_1 = pd.Series(months_days, index=["January", "March", "May", "July", "August", "October"])
+print(var_1)  
+
