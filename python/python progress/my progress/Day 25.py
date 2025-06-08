248b1d5989ee12c0e604e1958e4306f57225ae61
Agrim Jain <agrimjain015@gmail.com>
2025-06-08 11:58:03 +0530

    commit

--- python/python progress/my progress/Day 25.py
+++ python/python progress/my progress/Day 25.py
@@ -1,8 +1,16 @@
-# with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
-#     data = file.readlines()
-#     print(data)
+# # with open (r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
+# #     data = file.readlines()
+# #     print(data)
 
-import csv
-with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
-    data = csv.reader(file)
-    print(data)
+# import csv
+# with open(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv", "r") as file:
+#     data = csv.reader(file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+import pandas as pd
+data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\my progress\weather_data.csv")
+print(data)
