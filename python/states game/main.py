2d7c1d7a98d8bb478d8eb5f07787c82866423718
Agrim Jain <agrimjain015@gmail.com>
2025-06-10 14:14:17 +0530

    commit

--- python/states game/main.py
+++ python/states game/main.py
@@ -8,18 +8,22 @@ screen.addshape(image_path)
 turtle.shape(image_path)
 screen.setup(width=725, height=491)
 
-answer_state = screen.textinput(title = "Guess the state" , prompt = "What's another state's name?").title()
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title = "Guess the state" , prompt = "What's another state's name?").title()
 
-data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\50_states.csv")
-state_list = data.state.to_list()
-print(state_list)
-if answer_state in state_list:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(state_data.x.item(), state_data.y.item())
-    t.write(state_data.state)
+    data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\50_states.csv")
+    state_list = data.state.to_list()
+    print(state_list)
+    if answer_state in state_list:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+    
+    
     
     
 # if answerr_state is one of the states in all the states of the 50_states.csv file
