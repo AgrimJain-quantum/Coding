de9e3888f1306cd77c4fcbf16b2066dd02306a8f
Agrim Jain <agrimjain015@gmail.com>
2025-06-10 11:53:56 +0530

    commit

--- python/states game/main.py
+++ python/states game/main.py
@@ -11,7 +11,17 @@ screen.setup(width=725, height=491)
 answer_state = screen.textinput(title = "Guess the state" , prompt = "What's another state's name?").title()
 
 data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\50_states.csv")
-
+state_list = data.state.to_list()
+print(state_list)
+if answer_state in state_list:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(state_data.x, state_data.y)
+    t.write(state_data.state)
+    
+    
 # if answerr_state is one of the states in all the states of the 50_states.csv file
         #if they got it right 
            #create a turtle to write the state name at the state's x and y coordinates
