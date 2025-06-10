57a82869c0f806cff30ec83149b15a1383689e66
Agrim Jain <agrimjain015@gmail.com>
2025-06-10 10:35:05 +0530

    commit

--- python/states game/main.py
+++ python/states game/main.py
@@ -1,17 +1,23 @@
 import turtle
+import pandas as pd
 
-# Set up the screen
 screen = turtle.Screen()
 screen.title("States Game")
-
-# Provide the image path directly
 image_path = r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\blank_states_img.gif"
-
-# Register the image as a turtle shape
 screen.addshape(image_path)
-
-# Create a turtle and set its shape
 turtle.shape(image_path)
+screen.setup(width=725, height=491)
 
-# Wait for user to click to close the window
+answer_state = screen.textinput(title = "Guess the state" , prompt = "What's another state's name?").title()
+
+data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\50_states.csv")
+
+# if answerr_state is one of the states in all the states of the 50_states.csv file
+        #if they got it right 
+           #create a turtle to write the state name at the state's x and y coordinates
+
+
+
+
+
 screen.exitonclick()
