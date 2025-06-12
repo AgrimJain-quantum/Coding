ccda5237018b9dc010ba7735aef8efd8d0feb3f5
Agrim Jain <agrimjain015@gmail.com>
2025-06-12 15:35:30 +0530

    commit

--- python/states game/main.py
+++ python/states game/main.py
@@ -1,37 +1,32 @@
 import turtle
-import pandas as pd
+import pandas
 
 screen = turtle.Screen()
-screen.title("States Game")
-image_path = r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\blank_states_img.gif"
-screen.addshape(image_path)
-turtle.shape(image_path)
-screen.setup(width=725, height=491)
+screen.title("U.S. States Game")
+image = r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
+data = pandas.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\50_states.csv")
+all_states = data.state.to_list()
 guessed_states = []
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(title = "Guess the state" , prompt = "What's another state's name?").title()
 
-    data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\states game\50_states.csv")
-    state_list = data.state.to_list()
-    print(state_list)
-    if answer_state in state_list:
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
         state_data = data[data.state == answer_state]
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state)
-    
-    
-    
-    
-# if answerr_state is one of the states in all the states of the 50_states.csv file
-        #if they got it right 
-           #create a turtle to write the state name at the state's x and y coordinates
-
-
-
-
-
-screen.exitonclick()
