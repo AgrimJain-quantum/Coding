43889a3c7cfe7150cd90e7ca9ba46769e9cc6754
Agrim Jain <agrimjain015@gmail.com>
2025-06-14 14:25:33 +0530

    commit

--- python/python progress/my progress/Day 26.py
+++ python/python progress/my progress/Day 26.py
@@ -1,12 +1,19 @@
-number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-new_number = [n + 1 for n in number]
-name = "John"
-letter  = [letter  for letter in name]
-print(new_number)
-print(letter)
-range_list = [num * 2 for num in range (1, 11)]
+# number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+# new_number = [n + 1 for n in number]
+# name = "John"
+# letter  = [letter  for letter in name]
+# print(new_number)
+# print(letter)
+# range_list = [num * 2 for num in range (1, 11)]
 
-names = ["alex", "bob", "charlie", "dave"]
-short_names = [name for name in names if len(name)]
-print(range_list)
-print(short_names)
+# names = ["alex", "bob", "charlie", "dave"]
+# short_names = [name for name in names if len(name)]
+# print(range_list)
+# print(short_names)
+
+x = [y for y in range(1, 11)]
+print(x)
+w = [(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
+print(w)
+e = [(x, y) for x in [1,2,3] for y in [3,1,4] if x == y]
+print(e)
