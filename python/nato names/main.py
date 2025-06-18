2087dd5a294483b0a43e2b7f3d0a29c2f3ede014
Agrim Jain <agrimjain015@gmail.com>
2025-06-18 21:00:33 +0530

    commit

--- python/nato names/main.py
+++ python/nato names/main.py
@@ -1,5 +1,12 @@
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# #TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+import pandas as pd
+data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\nato names\nato_phonetic_alphabet.csv")
+print(data.to_dict())
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
+word = input("enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
