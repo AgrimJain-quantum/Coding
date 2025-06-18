417f9e524b5abb6acc79a8b9bf0153645b4a46e2
Agrim Jain <agrimjain015@gmail.com>
2025-06-18 21:02:01 +0530

    commit

--- python/nato names/main.py
+++ python/nato names/main.py
@@ -4,9 +4,7 @@
 
 import pandas as pd
 data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\nato names\nato_phonetic_alphabet.csv")
-print(data.to_dict())
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
-
 word = input("enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
