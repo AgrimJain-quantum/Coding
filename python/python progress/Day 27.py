7d301fe8b04f710556f698719c1e0478db13c001
Agrim Jain <agrimjain015@gmail.com>
2025-06-19 00:14:46 +0530

    commit

--- python/python progress/Day 27.py
+++ python/python progress/Day 27.py
@@ -2,7 +2,31 @@ import tkinter
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
-while True:
-    window.update()
-    window.mainloop()
+
+#label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
+my_label.pack(expand = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tur = turtle.Turtle()
+tur.write()
+
+
+
+window.mainloop()
     
