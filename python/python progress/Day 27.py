484b66094d85b7f093b3bc23582a8b8c44575dfc
Agrim Jain <agrimjain015@gmail.com>
2025-06-19 23:16:44 +0530

    commit

--- python/python progress/Day 27.py
+++ python/python progress/Day 27.py
@@ -1,18 +1,25 @@
-import tkinter 
-window = tkinter.Tk()
+import tkinter as tk
+from tkinter import Button
+window = tk.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 #label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
+my_label = tk.Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
 my_label.pack(expand = True)
+my_label["text"] = "New Text"
+my_label.config(text = "New Text Again")
+
+#button
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text="I got clicked")
 
 
-
-
-
-
-
+    
+button = Button(text = "Click Me", command=button_clicked, font=("Arial", 24, "bold", "italic"))
+button.pack(expand = True)
 
 
 
