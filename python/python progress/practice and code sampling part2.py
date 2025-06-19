ce81ee75be967e0238d94b13c24890515baf8fef
Agrim Jain <agrimjain015@gmail.com>
2025-06-19 22:03:08 +0530

    commit

--- python/python progress/practice and code sampling part2.py
+++ python/python progress/practice and code sampling part2.py
@@ -76,7 +76,28 @@
 # my_car = Car(make="Nissan", model="GTR",  color="Red", seats=4)
 # print(my_car.seats)
 
-def bar(spam, eggs, toast='yes please!', ham=0):
-    print(spam, eggs, toast, ham)
+# def bar(spam, eggs, toast='yes please!', ham=0):
+#     print(spam, eggs, toast, ham)
  
-bar(1, 2)
+# bar(1, 2)
+
+# def bar(spam, eggs, toast='yes please!', ham=0):
+#     print(spam, eggs, toast, ham)
+ 
+# bar(toast='nah', spam=4, eggs=2)
+
+# def test(*args):
+#     print(args)
+ 
+# test(1,2,3,5)
+
+# def test(*args):
+#     print(args)
+ 
+# test(1,2,3,5)
+
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+ 
+all_aboard(4, 7, 3, 0, x=10, y=64)
+
