3c6104ef4ee790330efce7b977379d03e4783816
Agrim Jain <agrimjain015@gmail.com>
2025-06-20 11:13:28 +0530

    commit

--- python/python progress/Day 27.py
+++ python/python progress/Day 27.py
@@ -6,7 +6,7 @@ window.minsize(width=500, height=300)
 
 #label
 my_label = tk.Label(text="I am a label", font=("Arial", 24, "bold", "italic"))
-my_label.pack(expand = True)
+my_label.pack(side = "left")
 my_label["text"] = "New Text"
 my_label.config(text = "New Text Again")
 
@@ -16,11 +16,11 @@ def button_clicked():
     new_text = input.get()
     my_label.config(text= new_text)       
 button = Button(text = "Click Me", command=button_clicked, font=("Arial", 24, "bold", "italic"))
-button.pack(side = "bottom", expand = True)
+button.pack(side = "bottom")
 
 #entry
 input = Entry(width = 10)
-input.pack(side = "right", expand = True)
+input.pack(side = "right")
 print(input.get()  ) 
 
 
