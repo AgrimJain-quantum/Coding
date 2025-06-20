71c56a7e9e71666e7c75a6ee7c0cea210ecfca8c
Agrim Jain <agrimjain015@gmail.com>
2025-06-20 21:25:16 +0530

    Add UI elements for miles to km converter
    Added input field, labels, and a calculate button to the Tkinter window for the miles to kilometers converter. Set up grid layout for all widgets and started the main event loop.

--- python/python progress/miles to km converter.py
+++ python/python progress/miles to km converter.py
@@ -1,5 +1,54 @@
 from tkinter import *
 
-window =    Tk()
+window = Tk()
 window.title("miles to km converter")
-window.minsize(width = 300, height = 200)
+
+
+miles_input = Entry(width = 10)
+miles_input.grid(column = 1 , row = 0 )
+
+miles_label = Label(text = "Miles")
+miles_label.grid(column = 2, row = 0)
+
+is_equal_label = Label(text = "is equal to")
+is_equal_label.grid(column = 0, row = 1)
+
+kilometer_result_label = Label(text = "0")
+kilometer_result_label.grid(column = 1 ,row = 1)
+
+kilometer_label = Label(text = "Km")
+kilometer_label.grid(column = 2, row = 1 )
+
+calculate_button = Button(text = "Calculate")
+calculate_button.grid(column = 1, row = 2 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
