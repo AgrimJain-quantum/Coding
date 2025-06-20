72d5b953755bee1b279158446f17fe983d0aa718
Agrim Jain <agrimjain015@gmail.com>
2025-06-20 22:24:36 +0530

    Fix miles to km conversion and button functionality
    Corrected the miles to kilometers conversion by casting input to float and rounding the result. Also connected the Calculate button to the conversion function.

--- python/python progress/miles to km converter.py
+++ python/python progress/miles to km converter.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
 def miles_to_km():
-    miles = miles_input.get()
-    km = miles * 1.60934
-    kilometer_result_label.config(text = km)
+    miles = float(miles_input.get())
+    km = round(miles * 1.60934)
+    kilometer_result_label.config(text = f"{km}")
 
 window = Tk()
 window.title("miles to km converter")
@@ -24,7 +24,7 @@ kilometer_result_label.grid(column = 1 ,row = 1)
 kilometer_label = Label(text = "Km")
 kilometer_label.grid(column = 2, row = 1 )
 
-calculate_button = Button(text = "Calculate")
+calculate_button = Button(text = "Calculate", command = miles_to_km )
 calculate_button.grid(column = 1, row = 2 )
 
 
