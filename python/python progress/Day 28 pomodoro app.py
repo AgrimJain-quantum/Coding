4042be4ebf1fbc275103fcd2b0fdfde324537925
Agrim Jain <agrimjain015@gmail.com>
2025-06-21 12:03:41 +0530

    Add initial UI setup for Pomodoro timer app
    Defined color constants, timer durations, and basic Tkinter window setup for the Pomodoro timer. Added a canvas and placeholder for the tomato image, preparing the structure for further UI and timer logic implementation.

--- python/python progress/Day 28 pomodoro app.py
+++ python/python progress/Day 28 pomodoro app.py
@@ -11,3 +11,32 @@
 #5. Create a reset button to reset the timer
 
 from tkinter import * 
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#f10b0b"
+GREEN = "#00ad2b"
+YELLOW = "#f3df04"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Timer")
+window.config(padx = 100, pady = 50, bg = YELLOW)
+canvas  = Canvas(width=200, height=224)
+canvas.create_image(100, 112, image = r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\tomato.png")
+
+
+
+
+
+
+window.mainloop()
