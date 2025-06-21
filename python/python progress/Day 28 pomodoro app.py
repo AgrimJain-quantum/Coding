5358af64a884f7dd2e80202417af88e1de19cf30
Agrim Jain <agrimjain015@gmail.com>
2025-06-21 21:27:06 +0530

    Add UI elements to Pomodoro app layout
    Introduced title label, canvas with clock image, start and reset buttons, and a check mark label to the Pomodoro timer app. These changes set up the basic user interface structure for the application.

--- python/python progress/Day 28 pomodoro app.py
+++ python/python progress/Day 28 pomodoro app.py
@@ -19,22 +19,30 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx = 100, pady = 50, bg = YELLOW)
- 
+
+#title 
 title_label = Label(text = "Timer", font = (FONT_NAME, 35, "bold"),fg =GREEN,bg = YELLOW)
 title_label.grid(column=1, row=0)
 
+#bg with clock
 canvas  = Canvas(width=200, height=224, bg = YELLOW, highlightthickness = 0 ) 
 PhotoImage = PhotoImage(file = r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\tomato.png")
 canvas.create_image(100, 112, image = PhotoImage)
 canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"), tag = "timer")
 canvas.grid(column=1, row=1)
 
+#buttons
+###start button
 start = Button(text = "Start", font = (FONT_NAME, 10, "bold"), command = None, highlightthickness = 0)
 start.grid(column = 0, row = 2)
 
+###stop button
 reset_button = Button(text = "reset", font = (FONT_NAME, 10, "bold"), command = None, highlightthickness = 0)
 reset_button.grid(column = 2, row = 2)
 
+#check mark
+check_mark = Label(text = "✓", fg = GREEN, bg = YELLOW, font = (FONT_NAME, 30, "bold"))
+check_mark.grid(column = 1, row = 3)
 
 
 
