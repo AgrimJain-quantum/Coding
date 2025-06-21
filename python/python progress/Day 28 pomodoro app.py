c9271badbc583863b6532c67c26d7bf75a896dc3
Agrim Jain <agrimjain015@gmail.com>
2025-06-21 21:19:44 +0530

    Increase timer label font and add start/reset buttons
    Updated the timer label to use a larger font size for better visibility. Added 'Start' and 'Reset' buttons to the UI, though their command callbacks are currently set to None.

--- python/python progress/Day 28 pomodoro app.py
+++ python/python progress/Day 28 pomodoro app.py
@@ -20,7 +20,7 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx = 100, pady = 50, bg = YELLOW)
  
-title_label = Label(text = "Timer", font = (FONT_NAME, 24, "bold"),fg =GREEN,bg = YELLOW)
+title_label = Label(text = "Timer", font = (FONT_NAME, 35, "bold"),fg =GREEN,bg = YELLOW)
 title_label.grid(column=1, row=0)
 
 canvas  = Canvas(width=200, height=224, bg = YELLOW, highlightthickness = 0 ) 
@@ -29,6 +29,11 @@ canvas.create_image(100, 112, image = PhotoImage)
 canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"), tag = "timer")
 canvas.grid(column=1, row=1)
 
+start = Button(text = "Start", font = (FONT_NAME, 10, "bold"), command = None, highlightthickness = 0)
+start.grid(column = 0, row = 2)
+
+reset_button = Button(text = "reset", font = (FONT_NAME, 10, "bold"), command = None, highlightthickness = 0)
+reset_button.grid(column = 2, row = 2)
 
 
 
