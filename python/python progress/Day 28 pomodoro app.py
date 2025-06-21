6c1e26a806e25a295b6aa22280ac82410b7f6219
Agrim Jain <agrimjain015@gmail.com>
2025-06-21 17:24:58 +0530

    Remove comments and add fg variable to Pomodoro app
    Removed initial TODO comments outlining the Pomodoro app flow and added a new 'fg' variable set to GREEN for future use in the UI.

--- python/python progress/Day 28 pomodoro app.py
+++ python/python progress/Day 28 pomodoro app.py
@@ -1,15 +1,3 @@
-#TODO
-#WE HAVE TO CREATE A POMODORO TIMER APP
-# FLOW OF THE APP
-#4 * 25 MINUTES WORK
-#3* 5 MINUTES BREAK
-#1 * 20 MINUTES BREAK
-#1. Create a window with a title
-#2. Create a canvas with a tomato image
-#3. Create a label to show the timer
-#4. Create a start button to start the timer
-#5. Create a reset button to reset the timer
-
 from tkinter import * 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -31,6 +19,7 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx = 100, pady = 50, bg = YELLOW)
+fg = GREEN
 canvas  = Canvas(width=200, height=224, bg = YELLOW, highlightthickness = 0 ) 
 PhotoImage = PhotoImage(file = r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\tomato.png")
 canvas.create_image(100, 112, image = PhotoImage)
