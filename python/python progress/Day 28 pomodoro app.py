2feaf9e8a3dbc0a7930e7e78a35db71e7bf61381
Agrim Jain <agrimjain015@gmail.com>
2025-06-21 08:44:23 +0530

    Add initial Pomodoro timer app files
    Added a new script with initial TODOs for a Pomodoro timer app and included a tomato image asset. Removed the previous Day 28 script to replace it with the new app structure.

---
+++ python/python progress/Day 28 pomodoro app.py
@@ -0,0 +1,13 @@
+#TODO
+#WE HAVE TO CREATE A POMODORO TIMER APP
+# FLOW OF THE APP
+#4 * 25 MINUTES WORK
+#3* 5 MINUTES BREAK
+#1 * 20 MINUTES BREAK
+#1. Create a window with a title
+#2. Create a canvas with a tomato image
+#3. Create a label to show the timer
+#4. Create a start button to start the timer
+#5. Create a reset button to reset the timer
+
+from tkinter import * 
