3c6e55b004b849790260999a1c932462a7728c81
Agrim Jain <agrimjain015@gmail.com>
2025-06-24 00:12:37 +0530

    Add password manager main.py and update tuple check
    Created a new main.py for the password manager with section headers. Updated DAY 29 password manager.py to check for 'apple' in a modified tuple and print the result.

--- python/python progress/DAY 29 password manager.py
+++ python/python progress/DAY 29 password manager.py
@@ -1,2 +1,7 @@
-thistuple = ("apple", "banana", "cherry")
-print(thistuple)
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple)
+thistuple = ( "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+else:
+  print("No, 'apple' is not in the fruits tuple")   
