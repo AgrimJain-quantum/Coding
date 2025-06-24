8613b0c34a10a01de3a8b6de7320f7bfbe005b1d
Agrim Jain <agrimjain015@gmail.com>
2025-06-24 14:15:51 +0530

    Add logo image and expand tuple unpacking examples
    Added a new logo.png file to the password manager directory. Updated the DAY 29 password manager.py script to include more tuple slicing and unpacking examples, demonstrating how to assign tuple elements to variables and print them.

--- python/python progress/DAY 29 password manager.py
+++ python/python progress/DAY 29 password manager.py
@@ -1,7 +1,28 @@
-# thistuple = ("apple", "banana", "cherry")
-# print(thistuple)
-thistuple = ( "banana", "cherry")
-if "apple" in thistuple:
-  print("Yes, 'apple' is in the fruits tuple")
-else:
-  print("No, 'apple' is not in the fruits tuple")   
+# # thistuple = ("apple", "banana", "cherry")
+# # print(thistuple)
+# # # thistuple = ( "banana", "cherry")
+# # # if "apple" in thistuple:
+# # #   print("Yes, 'apple' is in the fruits tuple")
+# # # else:
+# # #   print("No, 'apple' is not in the fruits tuple")   
+# # print(thistuple[-1])
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[1:2])
+# print(thistuple[-4:-1])
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+
+numbers = (1, 2, 3, 4, 5)
+(one, two, three, four, five) = numbers
+print(one)
+print(two)
+print(three)
+print(four)
+print(five)
