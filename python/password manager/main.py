31ee98f7dae066b4fa83b28d519067ca57681942
Agrim Jain <agrimjain015@gmail.com>
2025-06-24 17:48:47 +0530

    commit

--- python/password manager/main.py
+++ python/password manager/main.py
@@ -5,10 +5,3 @@ from tkinter import *
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
-canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(r"C:\Users\Agrim Jain\Desktop\Coding\python\password manager\logo.png")
-canvas.create_image(100, 100, image=r"C:\Users\Agrim Jain\Desktop\Coding\python\password manager\logo.png")
-canvas.grid(column=1, row=0)
