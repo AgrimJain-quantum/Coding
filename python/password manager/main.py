36d7ea2695f0cb029bbcfb13230c17bfb3538293
Agrim Jain <agrimjain015@gmail.com>
2025-06-24 17:46:35 +0530

    Set up basic UI for password manager app
    Initialized the Tkinter window with padding, added a canvas, and attempted to display a logo image. This sets up the foundation for the application's user interface.

--- python/password manager/main.py
+++ python/password manager/main.py
@@ -1,9 +1,14 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-import tkinter as tk    
-import random
-import string
-from tkinter import *
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+canvas = Canvas(width=200, height=200)
+logo_img = PhotoImage(r"C:\Users\Agrim Jain\Desktop\Coding\python\password manager\logo.png")
+canvas.create_image(100, 100, image=r"C:\Users\Agrim Jain\Desktop\Coding\python\password manager\logo.png")
+canvas.grid(column=1, row=0)
