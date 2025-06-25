334f7035ca0450ae4589fab5d160fdea903da076
Agrim Jain <agrimjain015@gmail.com>
2025-06-25 09:15:53 +0530

    Adjust layout and widths in password manager UI
    Updated label and entry widget widths for better alignment and appearance. Added sticky alignment to labels, entries, and buttons to improve UI consistency.

--- python/python progress/DAY 29 password manager.py
+++ python/python progress/DAY 29 password manager.py
@@ -10,29 +10,29 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 #labels
-website_label = Label(text = "Website: ")
-email_label = Label(text = "Email/Username: ")
-password_label = Label(text = "Password: ")
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1, sticky="W")
 
-website_label.grid(column=0, row=1)
-email_label.grid(column=0, row=2)
-password_label.grid(column=0, row=3)
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2, sticky="W")
 
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3, sticky="W")
 #entries
-website_entry = Entry(width=35)
-email_entry = Entry(width=35)
+website_entry = Entry(width=43)
+email_entry = Entry(width=43)
 password_entry = Entry(width=21)
 
-website_entry.grid(column=1, row=1, columnspan = 2)
-email_entry.grid(column=1, row=2, columnspan = 2)
-password_entry.grid(column=1, row=3)
+website_entry.grid(column=1, row=1, columnspan = 2, sticky = "W")
+email_entry.grid(column=1, row=2, columnspan = 2, sticky = "W")
+password_entry.grid(column=1, row=3, sticky = "W")
 
 #buttons
 generate_password_button = Button(text = "Generate Password")
 add_button = Button(text = "Add", width=36)
 
-generate_password_button.grid(column=2, row=3)
-add_button.grid(column=1, row=4, columnspan=2)
+generate_password_button.grid(column=2, row=3, sticky="W")
+add_button.grid(column=1, row=4, columnspan=2, sticky="W")
 
 
 
