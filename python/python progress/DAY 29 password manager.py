5c93250fc0cdfd9fc17bc5099517f64f9cc940e3
Agrim Jain <agrimjain015@gmail.com>
2025-06-25 11:14:30 +0530

    Improve UI defaults in password manager app
    Increased window padding for better spacing, adjusted password entry width, set initial focus to website entry, and pre-filled email entry with a default address to enhance user experience.

--- python/python progress/DAY 29 password manager.py
+++ python/python progress/DAY 29 password manager.py
@@ -2,7 +2,7 @@
 from tkinter import *
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.config(padx=70, pady=70)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file=r"C:\Users\Agrim Jain\Desktop\Coding\python\python progress\logo_image.png")
@@ -21,12 +21,14 @@ password_label.grid(column=0, row=3, sticky="W")
 #entries
 website_entry = Entry(width=43)
 email_entry = Entry(width=43)
-password_entry = Entry(width=23)
+password_entry = Entry(width=24)
 
 website_entry.grid(column=1, row=1, columnspan = 2, sticky = "W")
 email_entry.grid(column=1, row=2, columnspan = 2, sticky = "W")
 password_entry.grid(column=1, row=3, sticky = "W")
 
+website_entry.focus()
+email_entry.insert(0, "agrimjain015@gmail.com")
 #buttons
 generate_password_button = Button(text = "Generate Password")
 add_button = Button(text = "Add", width=36)
