5b602c1a95131a5569bf4bb8878ea95ee87cb75f
Agrim Jain <agrimjain015@gmail.com>
2025-06-26 12:25:16 +0530

    Comment out file handling and add tuple multiplication examples
    The previous file handling code is now commented out. Added examples demonstrating tuple multiplication and printing the results.

--- python/python progress/Day 30.py
+++ python/python progress/Day 30.py
@@ -1,8 +1,25 @@
 
-try:
-    file = open("a_day30.txt")
-except:
-    file = open("a_day30.txt", "w")
-    file.write("This is a new file created by the code.")
-    file.close()
-    file = open("a_day30.txt") 
+# # try:
+# #     file = open("a_day30.txt")
+# # except:
+# #     file = open("a_day30.txt", "w")
+# #     file.write("This is a new file created by the code.")
+# #     file.close()
+# #     file = open("a_day30.txt") 
+    
+# # numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
+# # (*green, tropic, red) = numbers
+# # print(green)
+# # print(tropic)
+# # print(red)
+
+
+# fruits = ("apple", "banana", "cherry")
+# mytuple = fruits * 2
+
+# print(mytuple)
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple2 * 3
+print(tuple3)
+//
