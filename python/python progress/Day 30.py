c6e7f7818c23bcb395e34c0dae56213cf51f5205
Agrim Jain <agrimjain015@gmail.com>
2025-06-28 17:39:36 +0530

    Refactor phonetic code to use a function
    Encapsulated the phonetic code generation logic into a generate_phonetic() function and invoked it at the end of the script. This improves code organization and reusability.

--- python/python progress/Day 30.py
+++ python/python progress/Day 30.py
@@ -72,10 +72,12 @@
 import pandas as pd
 data = pd.read_csv(r"C:\Users\Agrim Jain\Desktop\Coding\python\nato names\nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-word = input("enter a word: ").upper()
-try:
-    output_list = [phonetic_dict[letter] for letter in word]
-except KeyError:
-    print("Sorry, only letters in the alphabet please.")
-else:
-    print(output_list)
+def generate_phonetic():
+    word = input("enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
+generate_phonetic()
