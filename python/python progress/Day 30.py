ad840506236ac3449567b40c9265a863acce1a2c
Agrim Jain <agrimjain015@gmail.com>
2025-06-28 09:13:38 +0530

    Remove file creation logic and add KeyError example
    Deleted the file creation code and related comments. Added a new example demonstrating a KeyError when accessing a non-existent key in a dictionary.

--- python/python progress/Day 30.py
+++ python/python progress/Day 30.py
@@ -1,33 +1,7 @@
+# # file not found
+# with open("Day 30.txt") as file:
+#     file.read()
 
-# # try:
-# #     file = open("a_day30.txt")
-# # except:
-# #     file = open("a_day30.txt", "w")
-# #     file.write("This is a new file created by the code.")
-# #     file.close()
-# #     file = open("a_day30.txt") 
-    
-# # numbers = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
-# # (*green, tropic, red) = numbers
-# # print(green)
-# # print(tropic)
-# # print(red)
-
-
-# fruits = ("apple", "banana", "cherry")
-# mytuple = fruits * 2
-
-# print(mytuple)
-# tuple2 = (1, 2, 3)
-
-# tuple3 = tuple2 * 3
-# print(tuple3)
-
-set1 = {'1', '2', '3'}
-print(set1)
-
-hisset = {"apple", "banana", "cherry"}
-print(hisset)
-
-set2 = {1,2,3,4,5}
-print(set2)
+#key error
+dict = {"key": "value"}
+value = dict["non_existent_key"]
