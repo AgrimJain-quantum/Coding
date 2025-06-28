9ea5b7133df989d5fbb1f4f3b0782f34a9466dd0
Agrim Jain <agrimjain015@gmail.com>
2025-06-28 14:32:49 +0530

    Refactor Day 30 script and update test files
    Deleted a_day30.txt and added a_file.txt with sample content. Refactored 'Day 30.py' to include improved exception handling examples and added a BMI calculator with input validation.

--- python/python progress/Day 30.py
+++ python/python progress/Day 30.py
@@ -1,20 +1,52 @@
-# # file not found
-# with open("Day 30.txt") as file:
-#     file.read()
+# # # file not found
+# # with open("Day 30.txt") as file:
+# #     file.read()
 
-#key error
-# dict = {"key": "value"}
-# value = dict["non_existent_key"]
+# #key error
+# # dict = {"key": "value"}
+# # value = dict["non_existent_key"]
 
-# #index error
-# list = [1, 2, 3]
-# value = list[5]
+# # #index error
+# # list = [1, 2, 3]
+# # value = list[5]
 
-#type error
-# text = "Hello"
-# value = text + 5
+# #type error
+# # text = "Hello"
+# # value = text + 5
 
-try:
-    file = open("Day 30.txt")
-except FileNotFoundError:
-    print("File not found. Please check the file path.")
+# try:
+#     file = open("Day 30.txt")
+# except :
+#     file = open("Day 30.txt", "w")
+#     file.write("This is a new file created because the original file was not found.")
+
+#Exception Handling
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error message.")
+
+#     file.close()
+#     print("File was closed.")
+# raise KeyError("This is an error message.")
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+if height > 3:
+    raise ValueError("Height cannot be greater than 3 meters.")
+
+
+
+bmi = weight / (height ** 2)
+print(f"Your BMI is {bmi}")
